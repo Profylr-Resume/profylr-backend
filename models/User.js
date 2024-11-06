8ef3991802c78a3200e2068e097a939be37008ad
hemantairnet <airnet.development@gmail.com>
2024-11-06 18:25:26 +0530

    completed auth

--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
 import mongoose, { Schema } from "mongoose";
-
+import bcrypt from "bcryptjs";
 
 const userSchema = new Schema({
 	name:{
@@ -22,6 +22,34 @@ const userSchema = new Schema({
 		}
 	]
 }); 
+
+/* The pre hook runs before the save event on the schema. 
+ This allows you to apply custom logic every time a document 
+ is saved, which is ideal for tasks like hashing a password.
+ schema.post()
+ post middleware runs after the document is saved or validated, 
+ and it’s useful for logging, cleaning up, or triggering events after certain actions.
+ */
+
+userSchema.pre("save",async (next)=>{
+
+	// isModified => it’s available automatically when you create a schema in Mongoose. 
+	if(!this.isModified("password")) {return next();}
+
+	try{
+		const salt = await bcrypt.genSalt(10);
+		this.password = await bcrypt.hash(this.password,salt);
+
+		return next();
+	}catch(err){
+		return next(err);
+	}
+});
+
+userSchema.methods.comparePassword = async (enteredPassword)=>{
+	return await bcrypt.compare(enteredPassword,this.password);
+};
+
 
 const USER = mongoose.model("User",userSchema);
 
--- users/user.logic.js
+++
@@ -1,3 +0,0 @@
-const register = (name,email,password)=>{
-
-};
