aa67ff7d3d56a6e060a6a08dc22fd5dcf363dd29
hemantairnet <airnet.development@gmail.com>
2024-11-21 16:18:53 +0530

    started resume endpoints

--- controllers/crud/resume.controller.js
+++ controllers/crud/resume.controller.js
@@ -1,6 +1,19 @@
+import { createResumeHandler } from "../../handlers/resume.handler";
+import { missingFieldsError } from "../../utils/errors.utils";
 
 
 export const createResume = async(req,res)=>{
+
+	const userId = req?.user?._id;
+	if(!userId){
+		return missingFieldsError(res,"did not recieved user id while creating resume");
+	}
+
+	const {success,error,savedResume} = await createResumeHandler(req.body);
+
+	if(!success){
+		return missingFieldsError(res, error);
+	}
 
 };
 
