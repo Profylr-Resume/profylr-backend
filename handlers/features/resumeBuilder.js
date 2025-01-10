20bc712ead6aa5ca176d0b4c5a7682fe4d0a8bde
hemantairnet <airnet.development@gmail.com>
2025-01-10 18:16:14 +0530

    done with get temaplates on basis of selected sections .

--- handlers/features/resumeBuilder.js
+++ handlers/features/resumeBuilder.js
@@ -4,9 +4,42 @@ import { createBuildInResumeHandler } from "../modelConnection/buildInResume.han
 import { createImportedResumeHandler } from "../modelConnection/importedResume.handler";
 import { createResumeHandler } from "../modelConnection/resume.handler";
 import { createResumeTimeline } from "../modelConnection/resumeTimeline.handler";
+import { getTemplatesHandler } from "../modelConnection/template.handler";
 
+export const getTemplatesWithGivenSections = expressAsyncHandler(async(selectedSectionsArr,categories,departments)=>{
 
-export const createResume = expressAsyncHandler( async({data,userId})=>{
+	   // Fetch templates based on the provided filters
+	const filterParams = { categories, departments };
+	const filteredTemplates = await getTemplatesHandler(filterParams);
+   
+	// Check if templates were successfully fetched
+	if (!filteredTemplates || !filteredTemplates.success) {
+		throw new ApiError(500, "Unable to fetch templates.");
+	}
+   
+	const allTemplates = filteredTemplates.data; // Array of templates
+
+	// Filter sections in each template based on the selected sections array
+	const filteredTemplatesWithSections = allTemplates.map((template) => {
+		if (Array.isArray(template.sections)) {
+			return {
+				...template,
+				sections: template.sections.filter((section) =>
+					selectedSectionsArr.includes(section.section)
+				)
+			};
+		}
+		return null;
+		// return template; // Return the template as is if no sections exist
+	});
+
+	if(filteredTemplatesWithSections){
+		return {success:true , data : filteredTemplatesWithSections };
+	}
+	return {success:false,data:null};
+});
+
+export const createResume = expressAsyncHandler( async(data,userId)=>{
 
 	/** Flow :
     1. build In resume / Imported Resume 
@@ -63,7 +96,7 @@ export const createResume = expressAsyncHandler( async({data,userId})=>{
 	throw new ApiError(403,"Build In resume or Imported resume instance is not yet created");
 });
  
-const updateResume = expressAsyncHandler(async({updatedData,userId})=>{
+const updateResume = expressAsyncHandler(async(updatedData,userId)=>{
 
 	const {title,description,isImported, resumeData, resumeType, status , tags ,starred }= updatedData;
 
--- routes/features/persona.routes.js
+++ routes/features/persona.routes.js
@@ -1,9 +1,6 @@
 import { Router } from "express";
-import { createPersona } from "../../controllers/persona.controller.js";
 
 const router = Router();
-
-router.post("/",createPersona);
 
 
 export default router;
